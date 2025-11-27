0cf5a27f8dac95e33cda139c1cd8180147c51f2e
leodvincci <lpenrose@depaul.edu>
2025-11-27 03:19:45 -0600

    Init Commit

---
+++ src/main/java/com/penrose/springkafkaconsumerhelloworld/SpringKafkaConsumerHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.penrose.springkafkaconsumerhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaConsumerHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaConsumerHelloWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/penrose/springkafkaconsumerhelloworld/SpringKafkaConsumerHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penrose.springkafkaconsumerhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaConsumerHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
